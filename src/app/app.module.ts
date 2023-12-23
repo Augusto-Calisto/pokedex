17939954eb5872d8b799ab545ed60140b988e5bc
Augusto Calisto <augustoaquino007@gmail.com>
2023-12-23 15:18:34 -0300

    Importando componentes Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
 	declarations: [
@@ -13,6 +18,9 @@ import { AppComponent } from './app.component';
     	BrowserModule,
 		BrowserAnimationsModule,
     	AppRoutingModule,
+		MatPaginatorModule,
+		MatInputModule,
+		MatButtonModule
   	],
 
   	bootstrap: [AppComponent]
